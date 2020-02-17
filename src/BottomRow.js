12aa2e84a100ba492619cbcce24adff8440691cc
Pergamene <smartz373@gmail.com>
2020-02-17 16:19:00 -0700

    second stretch, increment quarter

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [quarter, incrementQuarter] = useState(1);
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +20,10 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
+        <button onClick={() => {
+          incrementQuarter(quarter + 1)}
+        }>+</button>
       </div>
     </div>
   );
